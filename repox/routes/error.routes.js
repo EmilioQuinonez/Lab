c3d5f022d3c717469a5427f971c3a14c73681ed6
EmilioQuinonez <150107914+EmilioQuinonez@users.noreply.github.com>
2025-03-10 21:54:28 -0600

    Delete repox directory

--- repox/routes/error.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const error_controller = require('../controllers/error.controller');
-
-router.get("*", error_controller.get_error);
-
-module.exports = router;
